1624b408734eb88717dd873f79ffa8f98bba8ac5
IanTavares <icltavares@hotmail.com>
2023-01-12 17:43:37 -0300

    Alterei o css e coloquei validações

--- script.js
+++ script.js
@@ -1,18 +1,37 @@
-var email = document.getElementById('email');
-var password = document.getElementById('password');
+const nameInput = document.querySelector("#name")
+const form = document.querySelector("#form")
+const emailInput = document.querySelector("#email");
+const passwordInput = document.querySelector("#password");
 
-email.addEventListener('focus',()=>{
-  email.style.borderColor = "#4a5f6a"; 
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+
+//Verifica se o nome está vazio
+if (nameInput.value === "") {
+  alert("Por favor, preencha o seu nome");
+  return
+}
+
+// Verifica se o email está preenchido e se é válido
+if (emailInput.value === ""){
+  alert("Por favor, preencha o seu email");
+  return;
+}
+
+// Se todos os campos estiverem corretamente preenchidos, envie o form
+form.submit();
 });
 
-email.addEventListener('blue',()=>{
-  email.style.borderColor = "#ccc"; 
-});
+//Função que valida email
+function isEmailValid(email){
 
-password.addEventListener('focus',()=>{
-  password.style.borderColor = "#4a5f6a"; 
-});
+  //cria uma regex para validar email
+  const emailRegex = new RegExp(
+    //usuario12
+    /^[a-zA-z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]/
+  )
 
-password.addEventListener('blue',()=>{
-  password.style.borderColor = "#ccc"; 
-});
+
+}
