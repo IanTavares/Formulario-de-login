b5bbf33d46dec295e771639de89a34f3dbcd1ac7
IanTavares <icltavares@hotmail.com>
2023-01-12 18:34:19 -0300

    Personalizei o css e coloquei validações

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ const form = document.querySelector("#form")
 const emailInput = document.querySelector("#email");
 const passwordInput = document.querySelector("#password");
 
-
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
@@ -20,18 +19,40 @@ if (emailInput.value === ""){
   return;
 }
 
+// Verifica se a senha está preenchida
+if(validatePassword(passwordInput.value)){
+  alert("A senha precisa ser no mínimo de 8 dígitos.");
+  return;
+
+
+}
+
 // Se todos os campos estiverem corretamente preenchidos, envie o form
 form.submit();
 });
 
 //Função que valida email
 function isEmailValid(email){
-
   //cria uma regex para validar email
   const emailRegex = new RegExp(
-    //usuario12
-    /^[a-zA-z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]/
-  )
+    //usuario12@host.com.br
+    /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,}$/
+  );
 
+  if (emailRegex.test(email)) {
+    return true;
+  }
 
+return false;
+}
+
+// Função que valida a senha
+function validatePassword(password, minDigits){
+  if(password.length >= minDigits) {
+    //Senha válida
+    return true
+  }
+
+  //Senha inválida
+  return false
 }
